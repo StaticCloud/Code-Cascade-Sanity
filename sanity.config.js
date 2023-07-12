60f506ed0a34da08c81c618fbb8ffb5ea869db63
DMarrs <diegomarrs@gmail.com>
2023-07-11 17:46:42 -0700

    initial commit

--- sanity.config.js
+++ sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'code-cascade-sanity',
 
-  projectId: 'c21vnf5l',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
 
   plugins: [deskTool(), visionTool()],
 
